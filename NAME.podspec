050a7191823a4444bdaa27f56b562ca4c75d5a3b
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2017-07-05 18:33:25 +0300

    - Adopting podspec for myself

--- NAME.podspec
+++ NAME.podspec
@@ -24,7 +24,7 @@ TODO: Add long description of the pod here.
   s.homepage         = 'https://github.com/${USER_NAME}/${POD_NAME}'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { '${USER_NAME}' => '${USER_EMAIL}' }
+  s.author           = { 'Anton Plebanovich' => 'anton.plebanovich@gmail.com' }
   s.source           = { :git => 'https://github.com/${USER_NAME}/${POD_NAME}.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
--- templates/swift/Example/PROJECT/ViewController.swift
+++ templates/swift/Example/PROJECT/ViewController.swift
@@ -2,23 +2,10 @@
 //  ViewController.swift
 //  PROJECT
 //
-//  Created by PROJECT_OWNER on TODAYS_DATE.
-//  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
+//  Created by Anton Plebanovich on TODAYS_DATE.
+//  Copyright (c) TODAYS_YEAR Anton Plebanovich. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-}
-
+class ViewController: UIViewController {}
