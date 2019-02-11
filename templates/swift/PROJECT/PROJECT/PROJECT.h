4c51d636407fb2f9d458e2dcaf5e212d3afd3fbe
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2019-02-11 12:58:57 +0300

    - Carthage project

---
+++ templates/swift/PROJECT/PROJECT/PROJECT.h
@@ -0,0 +1,19 @@
+//
+//  PROJECT.h
+//  PROJECT
+//
+//  Created by Anton Plebanovich on 2/11/19.
+//  Copyright © 2019 Anton Plebanovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PROJECT.
+FOUNDATION_EXPORT double PROJECTVersionNumber;
+
+//! Project version string for PROJECT.
+FOUNDATION_EXPORT const unsigned char PROJECTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PROJECT/PublicHeader.h>
+
+
