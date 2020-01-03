950f5110162f1669ee4c4678926f1efdd7570e58
Anton Plebanovich <anton.plebanovich@gmail.com>
2020-01-03 10:42:56 +0300

    - Try remove ReplaceMe

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -26,10 +26,6 @@ module Pod
 
       `mv ./templates/swift/* ./`
       
-      # There has to be a single file in the Classes dir
-      # or a framework won't be created
-      `touch Pod/Classes/ReplaceMe.swift`
-      
       # The Podspec should be 8.0 instead of 7.0      
       text = File.read("NAME.podspec")
       text.gsub!("7.0", "8.0")
